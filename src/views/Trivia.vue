a94d61c5ed48f6fae12b3734469c3fb97f871ba4
Adewale Shotobi <sholekbiz@gmail.com>
2024-06-28 00:10:45 +0100

    revert api routes

--- src/views/Trivia.vue
+++ src/views/Trivia.vue
@@ -65,7 +65,7 @@ export default {
     };
     // load questions
     let response = await fetch(
-      import.meta.env.VITE_API_BASE_URL + "/api/v1/trivia/start",
+      import.meta.env.VITE_API_BASE_URL + "/api/v1/start-trivia",
       requestOptions
     ).then(this.handleResponse);
     console.log(response);
@@ -126,7 +126,7 @@ export default {
       };
       // load questions
       let response = await fetch(
-        import.meta.env.VITE_API_BASE_URL + "/api/v1/trivia/submit",
+        import.meta.env.VITE_API_BASE_URL + "/api/v1/submit",
         requestOptions
       ).then(this.handleResponse);
 
