6a8304dc7bce986f3ded30c85e46de19126f807a
Adewale Shotobi <sholekbiz@gmail.com>
2024-06-28 17:18:42 +0100

    switched to has history

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Result from '@/views/Result.vue'
 import Index from '@/views/Index.vue'
 import Trivia from '@/views/Trivia.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
